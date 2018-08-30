f19064f7bab5403fa939b017b1afe6fa7ab16a7d
Kattoor <jasper.catthoor@gmail.com>
2018-08-30 10:01:48 +0200

    Fixed bug in decompression of consecutively repeating characters

--- src/Main.java
+++ src/Main.java
@@ -59,20 +59,18 @@ public class Main {
 
         List<String> dictionary = initializeDictionary();
 
-        int previousIndex = input.get(0);
-        String previousValue = dictionary.get(previousIndex);
+        String previousValue = dictionary.get(input.get(0));
         output.append(previousValue);
 
         for (int currentIndex : input.subList(1, input.size())) {
-            String currentValue;
-            if (currentIndex == dictionary.size())
-                currentValue = previousValue + previousValue.charAt(0);
-            else
-                currentValue = dictionary.get(currentIndex);
+            String currentValue = currentIndex == dictionary.size() ?
+                    previousValue + previousValue.charAt(0) :
+                    dictionary.get(currentIndex);
+
             output.append(currentValue);
-            char firstCharOfCurrentValue = currentValue.charAt(0);
-            dictionary.add(dictionary.get(previousIndex) + firstCharOfCurrentValue);
-            previousIndex = currentIndex;
+            dictionary.add(previousValue + currentValue.charAt(0));
+
+            previousValue = currentValue;
         }
 
         return output.toString();
